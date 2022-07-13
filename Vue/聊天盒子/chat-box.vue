327bbc4f89d48517cfb0e16eba05f60fb42337c9
xiaoqinvar <17683860320m0@sina.cn>
2022-07-13 16:07:01 +0800

    update componentes.

--- Vue/聊天盒子/chat-box.vue
+++ Vue/聊天盒子/chat-box.vue
@@ -20,13 +20,28 @@
     <!--  聊天框  -->
     <div class="chat-input">
       <div class="emoji">
-        <svg class="icon" aria-hidden="true">
+
+        <!--    emoji表情栏    -->
+        <VEmojiPicker
+            ref="emojiRef"
+            v-show="showEmoji"
+            @click.native.stop=""
+            @select="selectEmoji" />
+        <svg class="icon"
+             aria-hidden="true"
+             @click.stop="openEmoji">
           <use xlink:href="#icon-xiaolian"></use>
         </svg>
       </div>
-      <el-input placeholder="请输入消息" v-model="input2">
+      <el-input :placeholder="disabled ? '请先登录' : '请输入消息'"
+                :value.sync="message"
+                :disabled="disabled"
+                @click.native.stop=""
+                @click.native="clickInput"
+                @keyup.native.enter="clickInput"
+                @input="(value) => $emit('update:message', value)">
         <template slot="append">
-          <div class="send">发送</div>
+          <div class="send" :class="{disabled: disabled}" @click="send">发送</div>
         </template>
       </el-input>
     </div>
@@ -35,30 +50,34 @@
 
 <script>
 // 组件
+import {VEmojiPicker} from 'v-emoji-picker';
 import chatItem from "components/chat-item/chat-item.vue";
 
 export default {
   name: "chat-box",
+  data() {
+    return {
+    };
+  },
   props: {
+    // 禁用输入框
+    disabled: Boolean,
     // 聊天数据列表
     chatList: {
       type: Array,
       default() {
-        return [
-          // 测试数据
-          {time: 1655435517560, color: 'orange', uid: 'xiaoqinvar', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill1', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill2', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill3', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill4', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill5', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill1', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill2', talk: 'lol...'},
-          {time: 1655435517560, color: 'orange', uid: 'jybill3', talk: 'lol...'},
-        ]
+        return [];
       }
     },
+
+    // 消息
+    message: String,
+
+    // 展示emoji选择板
+    showEmoji: {
+      type: Boolean,
+      default: false,
+    }
   },
   methods: {
     /**
@@ -79,15 +98,68 @@ export default {
       return new Date(timeStamp).toLocaleTimeString();
     },
 
+    /**
+     * 未登录状态下点击input会触发toLogin事件
+     */
+    clickInput(key) {
+      if (!this.disabled) {
+        if (key.keyCode === 13) {
+          this.send();
+        }
+        return;
+      }
+      this.$emit('toLogin');
+    },
+
+    /**
+     * 登陆状态下发送消息事件
+     */
+    send() {
+      if (this.disabled) {
+        return;
+      }
+      this.$emit('sendMessage', this.message);
+      this.$emit('update:showEmoji', false);
+    },
+
+    /**
+     * 打开emoji
+     */
+    openEmoji() {
+      if (this.disabled) {
+        this.$emit('toLogin');
+        return;
+      }
+      this.$emit('update:showEmoji', !this.showEmoji);
+    },
+
+    /**
+     * 选择emoji表情
+     */
+    selectEmoji(emoji) {
+      if (this.disabled) {
+        return;
+      }
+
+      const data = emoji.data;
+      this.$emit('update:message', this.message + data);
+    },
   },
 
   components: {
     chatItem,
+    VEmojiPicker
   },
   watch: {
     chatList() {
-      this.$nextTick(() => this.$refs["aside"].scrollTop = this.$refs["chat"].scrollHeight);
+      this.$nextTick(() => {
+        this.$refs["chat"].scrollTop = this.$refs["chat"].scrollHeight;
+      });
     }
+  },
+  mounted() {
+    // 初始化表情选项
+    // const emojiRef = this.$refs['emojiRef'].$children[1];
   },
 };
 </script>
--- Vue/输入框+按钮/input-button.vue
+++ Vue/输入框+按钮/input-button.vue
@@ -1,22 +1,52 @@
 <template>
   <div class="input-button">
-    <el-input ref="elInputRef" v-model="content" class="input" :placeholder="placeholderContent ? placeholderContent : '请输入内容'" @focus="clickInput" @keyup.native.enter="clickSend"></el-input>
+    <VEmojiPicker
+        v-show="showEmoji"
+        @select="selectEmoji"
+        @click.native.stop=""/>
+    <svg class="icon" aria-hidden="true" @click.stop="openEmoji">
+      <use xlink:href="#icon-xiaolian"></use>
+    </svg>
+    <el-input ref="elInputRef"
+              v-model="content"
+              class="input"
+              :placeholder="placeholderContent ? placeholderContent : '请输入内容'"
+              @focus="clickInput"
+              @keyup.native.enter="clickSend"
+              @click.native.stop=""
+    ></el-input>
     <el-button @click="clickSend">发送</el-button>
   </div>
 </template>
 
 <script>
+import {VEmojiPicker} from 'v-emoji-picker';
   export default {
     name: "input-button",
     props: {
       // 提示信息
-      placeholderContent: String | null,
+      placeholderContent: {
+        type: String,
+        default: null
+      },
+
+      // 是否登陆
+      isLogin: {
+        type: Boolean,
+        default: false,
+      },
+
+      // 展示emoji选择板
+      showEmoji: {
+        type: Boolean,
+        default: false,
+      },
     },
     data() {
       return {
         // 评论内容
         content: '',
-      }
+      };
     },
     methods: {
       /**
@@ -30,30 +60,38 @@
        * 组件事件：点击发送按钮发送消息, 并清空内容
        */
       clickSend() {
-        if (!this.content || this.content.trim().length <= 0) {
-          this.$message.warning("不能发送空内容");
-          return;
-        }
         this.$emit('sendMessage', this.content);
+        this.$emit('update:showEmoji', false);
         this.content = '';
       },
 
       /**
-       *
-       *
-       * 封装
-       *
-       *
+       * 打开emoji板块
        */
-      /**
-       * 从storage中获取用户信息，用户登陆true，否则false
-       * @return Boolean
-       */
-      getUserByLocalStorage() {
+      openEmoji() {
+        if (!this.isLogin) {
+          this.$emit('notLogin');
+          return;
+        }
+        this.$emit('update:showEmoji', !this.showEmoji);
+      },
 
-      }
+      /**
+       * 选择emoji表情
+       */
+      selectEmoji(emoji) {
+        if (!this.isLogin) {
+          this.$emit('notLogin');
+          return;
+        }
+        const data = emoji.data;
+        this.content += data;
+      },
     },
-  }
+    components: {
+      VEmojiPicker
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
