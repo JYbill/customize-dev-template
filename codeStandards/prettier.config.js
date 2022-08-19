646ea3fdcca1e818ce19071cba64208f8d4bec33
xiaoqinvar <17683860320m0@sina.cn>
2022-08-19 10:59:04 +0800

    adding config of code standard

---
+++ codeStandards/prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss'), require('prettier-plugin-packagejson')],
+  singleQuote: true,
+  printWidth: 180,
+  arrowParens: 'always'
+};
