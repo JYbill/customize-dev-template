f20ca74fb3f6877828a0b985fa7ad510ac347b83
xiaoqinvar <17683860320m0@sina.cn>
2022-11-28 16:48:28 +0800

    feat: prettier-plugin: prettier-plugin-sort-imports

--- 代码格式标准/.prettierrc.js
+++ 代码格式标准/.prettierrc.js
@@ -2,7 +2,22 @@ module.exports = {
   semi: true,
   tabWidth: 2,
   printWidth: 120,
-  proseWrap: 'preserve',
+  proseWrap: "preserve",
   singleQuote: false,
-  trailingComma: 'all',
-}
+  trailingComma: "all",
+
+  // 插件
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+
+  // prettier-plugin-sort-imports 排序import内容
+  importOrder: [
+    // 默认情况下，首先会放置外部依赖项
+    "^react(.*)$",
+    "^axios$",
+
+    // 内部依赖
+    "^@/(.*)$",
+  ],
+  importOrderSeparation: true, // 每个类别导入用空格区分
+  importOrderSortSpecifiers: true, // 大小写排序
+};
