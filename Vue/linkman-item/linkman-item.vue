7827547b6617c6b1584e0c21268b6e57e4784c06
xiaoqinvar <17683860320m0@sina.cn>
2022-12-28 11:48:25 +0800

    feat: add vue2 components.

---
+++ Vue/linkman-item/linkman-item.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="linkman-item">
+    <div class="header">{{ username[0] }}</div>
+    <div class="name">{{ username }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "linkman-item",
+  props: {
+    username: String,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "linkman-item";
+</style>
