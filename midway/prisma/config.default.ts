88ce04d57de2d8f74489dc004e9843a73d26b35e
xiaoqinvar <17683860320m0@sina.cn>
2022-12-31 16:57:42 +0800

    fix: midway prisma.

--- midway/prisma/config.default.ts
+++ midway/prisma/config.default.ts
@@ -1,14 +1,23 @@
+import type { Prisma } from "@prisma/client";
+
 import { MidwayConfig } from "@midwayjs/core";
 
-// 服务工厂一定要配置实例，这里推荐多例
+// prisma配置
+const prismaDefaultConf: Prisma.PrismaClientOptions = {
+  log: [
+    { level: "query", emit: "event" },
+    { emit: "stdout", level: "error" },
+    { emit: "stdout", level: "info" },
+    { emit: "stdout", level: "warn" },
+  ],
+};
 const prismaConfig = {
-  default: {},
+  default: prismaDefaultConf,
   clients: {
     mongo: {},
     mysql: {},
   },
 };
-export type prismaConfigType = typeof prismaConfig;
 
 export default {
   // use for cookie sign key, should change to your own and keep security
