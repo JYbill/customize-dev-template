b9e3c2f6694c916028ec0ecb7a3dd36d2b5217be
xiaoqinvar <17683860320m0@sina.cn>
2023-01-17 15:28:15 +0800

    fix: 更新midway整合passport策略

--- midway/filter/notfound.filter.ts
+++ midway/filter/notfound.filter.ts
@@ -1,11 +1,13 @@
-import { MidwayHttpError, httpError } from "@midwayjs/core";
+import Res from "../vo/res.vo";
+
+import { HttpStatus, MidwayHttpError, httpError } from "@midwayjs/core";
 import { Catch } from "@midwayjs/decorator";
 import { Context } from "@midwayjs/koa";
-import Res from "../vo/res.vo";
 
 @Catch(httpError.NotFoundError)
 export class NotFoundFilter {
   async catch(err: MidwayHttpError, ctx: Context) {
+    ctx.status = HttpStatus.NOT_FOUND;
     ctx.body = Res.error("404 Not Found");
   }
 }
