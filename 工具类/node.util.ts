38947c786d7a58d0f4a120567cdfa1cc072a341e
xiaoqinvar <17683860320m0@sina.cn>
2023-05-15 17:43:25 +0800

    feat：node util

--- 工具类/node.util.ts
+++ 工具类/node.util.ts
@@ -46,14 +46,39 @@ export class NodeUtil {
   }
 }
 
-export async function saveUploadFile(basedir, savePath, readableStream): Promise<boolean> {
-  const [filename, suffix] = readableStream.filename.split(".");
-  const randomFilename = filename + "." + Math.random().toString(16).slice(2) + "." + suffix;
-  // 流写入
-  const filepath = path.resolve(basedir, savePath, randomFilename);
-  const ws = fs.createWriteStream(filepath);
-  fileStream.pipe(ws);
-  await new Promise((resolve) => {
-    ws.on("finish", () => resolve(true));
+/**
+ * 存储上传的文件流
+ * @param readableStream
+ * @returns
+ */
+export async function saveUploadFile(readableStream): Promise<boolean> {
+  return new Promise((resolve) => {
+    const baseDir = this.baseDir;
+    const splitList = readableStream.filename.split(".");
+    const suffix = splitList[splitList.length - 1];
+    const filename = splitList.reduce((prev, curr, index) => {
+      if (index === 0) {
+        return curr;
+      } else if (index === splitList.length - 1) {
+        return prev;
+      }
+      return prev + "." + curr;
+    }, "");
+    const randomFilename = filename + "." + Math.random().toString(16).slice(2) + "." + suffix;
+    // 流写入
+    const filepath = path.resolve(baseDir, "app", this.uploadTemp, randomFilename);
+    const ws = fs.createWriteStream(filepath);
+    ws.on("close", () =>
+      resolve({
+        filepath,
+        originFileName: readableStream.filename,
+        filename: randomFilename,
+      }),
+    );
+    ws.on("error", (err) => {
+      this.ctx.logger.error(err);
+      this.ctx.throw(400, "saveUploadFile()#文件本地存储错误");
+    });
+    readableStream.pipe(ws);
   });
 }
