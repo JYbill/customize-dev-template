0312870f25e5678d3b004cbfde9f0d152ca14f05
xiaoqinvar <17683860320m0@sina.cn>
2023-11-15 10:33:30 +0800

    feat: 更新多个工具文件

--- 工具类/bom.util.ts
+++ 工具类/bom.util.ts
@@ -21,7 +21,7 @@ export async function copyTxt(text: string): Promise<void> {
     textarea.style.position = "absolute";
     textarea.style.zIndex = "-1";
     textarea.style.opacity = "0";
-    textarea.value = text;
+    textarea.value = text || "";
     textarea.select();
     document.execCommand("copy", true);
     document.body.removeChild(textarea);
@@ -57,6 +57,32 @@ export async function fetchDownloadBlob(apiUrl, query) {
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+}
+
+/**
+ * 通过xhr下载二进制文件并用a标签保存到本地
+ * @param url
+ * @param name 文件名
+ */
+export function downVideo(url, name) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "arraybuffer"; // 返回类型blob
+  xhr.onload = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let blob = this.response;
+      // 转换一个blob链接
+      let u = window.URL.createObjectURL(new Blob([blob], { type: "video/mp4" })); // 视频的type是video/mp4，图片是image/jpeg
+      let a = document.createElement("a");
+      a.download = name; // 设置的文件名
+      a.href = u;
+      a.style.display = "none";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }
+  };
+  xhr.send();
 }
 
 /**
