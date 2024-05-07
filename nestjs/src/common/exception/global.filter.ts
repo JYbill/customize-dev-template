5894fe8d46bd99527fcf8f00deb459866c169a17
xiaoqinvar <17683860320m0@sina.cn>
2024-05-07 10:08:29 +0800

    feat(nest): 全局拦截器更新

--- nestjs/src/common/exception/global.filter.ts
+++ nestjs/src/common/exception/global.filter.ts
@@ -1,6 +1,7 @@
 import { ResponseUtil } from '../util/response.util';
 import {
   ArgumentsHost,
+  BadRequestException,
   Catch,
   ExceptionFilter,
   HttpException,
@@ -12,7 +13,7 @@ import { Request, Response } from 'express';
 /**
  * @Description: 全局异常处理器
  * @Author: 小钦var
- * @Date: 2023/10/12 10:42
+ * @Date: 2024年05月07日 10:07:48
  */
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter<HttpException> {
@@ -24,16 +25,24 @@ export class GlobalExceptionFilter implements ExceptionFilter<HttpException> {
     const response = ctx.getResponse<Response>();
 
     request.pass = false;
-    this.logger.error(exception.stack);
+
     try {
       // 正常业务代码抛出的异常
-      const status = exception.getStatus();
       if (exception instanceof NotFoundException) {
+        this.logger.warn(exception.message);
+        const status = exception.getStatus();
         response.status(status).json(ResponseUtil.error('接口未找到', status));
+      } else if (exception instanceof BadRequestException) {
+        this.logger.warn(`request error: ${exception.message}`);
+        const status = exception.getStatus();
+        response
+          .status(status)
+          .json(ResponseUtil.error('请求格式错误', status));
       } else {
+        this.logger.error(exception.stack);
+        const status = exception.getStatus();
         response.status(status).json(ResponseUtil.error(undefined, status));
       }
-      return;
     } catch (err: unknown) {
       const error = err as Error;
       // 代码错误
