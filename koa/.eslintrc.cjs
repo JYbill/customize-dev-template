05b3e70b4d321f174fb1673fc33bb0689c46d1ca
xiaoqinvar <17683860320m0@sina.cn>
2024-07-03 17:52:25 +0800

    feat: koa模板更新

--- koa/.eslintrc.cjs
+++ koa/.eslintrc.cjs
@@ -4,12 +4,18 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:n/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
+    "no-duplicate-imports": ["error"], // 不允许重复导入相同的模块
     "no-unused-vars": "off",
+    "n/file-extension-in-import": ["error", "always"], // 导入相对路径的文件必须指明扩展名
   },
 };
---
+++ koa/prettier.config.js
@@ -0,0 +1,21 @@
+export default {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrderSeparation: true, // 导入种类用空行分隔
+  importOrderSortSpecifiers: true, // 大小写排序
+  importOrderParserPlugins: ["classProperties"],
+  importOrder: [
+    "./config/env-loader.js", // ESM下使用dotenv加载环境变量必须放在首位
+    "^@koa",
+    "^koa",
+    "^node:(.*)$",
+    "^#",
+    "^[./]",
+  ],
+
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  proseWrap: "preserve",
+  singleQuote: false,
+  trailingComma: "all",
+};
