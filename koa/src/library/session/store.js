e3a7913728d2c3afc9220f812dbb65591f94d05a
xiaoqinvar <17683860320m0@sina.cn>
2024-08-20 14:20:26 +0800

    fix(koa): redis store

--- koa/src/library/session/store.js
+++ koa/src/library/session/store.js
@@ -6,15 +6,32 @@ export const RedisStore = {
     return JSON.parse(session);
   },
   async set(key, value, maxAge, { changed, ctx, rolling }) {
-    if (ctx.session.isNew) {
-      // 新建操作
-      return await GlobalRedis.psetex(key, maxAge, JSON.stringify(value));
+    try {
+      const exist = await GlobalRedis.exists(key);
+      if (!exist) {
+        // 新建操作
+        const result = await GlobalRedis.psetex(
+          key,
+          maxAge,
+          JSON.stringify(value),
+        );
+        return result;
+      }
+      // 更新操作
+      const ms = await GlobalRedis.pttl(key);
+      // 此时session已过期TTL被清理，不做任何操作
+      if (ms <= 0) {
+        return;
+      }
+      // 🚨 务必保证TTL的原子性，否则会出现意外过期的错误
+      const result = await GlobalRedis.psetex(key, ms, JSON.stringify(value));
+      return result;
+    } catch (err) {
+      // 意外错误兜底
+      logger.error("key: %s, maxAge: %s", key, maxAge);
+      logger.error("session store error: %s", err);
+      throw err;
     }
-    // 更新操作
-    const ms = await GlobalRedis.pttl(key);
-    // 🚨 务必保证TTL的原子性，否则会出现意外过期的错误
-    const result = await GlobalRedis.psetex(key, ms, JSON.stringify(value));
-    return result;
   },
   async destroy(key, { ctx }) {
     const affected = await GlobalRedis.del(key);
