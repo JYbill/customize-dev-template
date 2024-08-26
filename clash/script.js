5e026f4ee22e911d2c67fc7004d64ab97e9452d4
xiaoqinvar <17683860320m0@sina.cn>
2024-08-26 16:42:28 +0800

    feat(clash): 优化脚本分流规则

--- clash/script.js
+++ clash/script.js
@@ -5,10 +5,13 @@
  * @return  {[type]}
  */
 function main(config, profileName) {
+    const activeProfileName = "🚀 节点选择";
+  
+    // 创建代理
     const proxyUSAList = config.proxies
       .filter(item => item.name.match(/新加坡/ig))
       .map(item => item.name);
-    const proxyUSAField = "🔫和平每一天";
+    const proxyUSAField = "🤖 AI专属"; // AI代理
     config["proxy-groups"].push({
       name: proxyUSAField,
       type: "url-test",
@@ -17,25 +20,29 @@ function main(config, profileName) {
       interval: 86400,
     });
     config["rules"].unshift(
-      `RULE-SET,applications,DIRECT`,
+      // 第一层：指定的域名分流（优先级最高）
       `DOMAIN,clash.razord.top,DIRECT`,
       `DOMAIN,yacd.haishan.me,DIRECT`,
+      `DOMAIN-KEYWORD,gemini,${proxyUSAField}`, // 这块好像没有生成成功，用域名代替
+      // 第二层：geosite分流
+      `GEOSITE,CN,DIRECT`,
+      `GEOSITE,gfw,${activeProfileName}`,
+      `GEOSITE,greatfire,${activeProfileName}`,
+      `GEOSITE,github,${activeProfileName}`,
+      `GEOSITE,openai,${proxyUSAField}`,
+      `GEOSITE,anthropic,${proxyUSAField}`,
+      // 第三层：rule-providers分流
+      `RULE-SET,applications,DIRECT`,
       `RULE-SET,private,DIRECT`,
       `RULE-SET,reject,REJECT`,
       `RULE-SET,icloud,DIRECT`,
       `RULE-SET,apple,DIRECT`,
-      // RULE-SET,google,代理出口(自行修改),
-      // RULE-SET,proxy,代理出口(自行修改),
-      // RULE-SET,telegramcidr,代理出口(自行修改),no-resolve,
       `RULE-SET,direct,DIRECT`,
       `RULE-SET,lancidr,DIRECT,no-resolve`,
       `RULE-SET,cncidr,DIRECT,no-resolve`,
-      `DOMAIN-KEYWORD,anthropic,${proxyUSAField}`,
-      `DOMAIN-KEYWORD,claude,${proxyUSAField}`,
-      `DOMAIN-KEYWORD,openai,${proxyUSAField}`,
-      `DOMAIN-KEYWORD,chatgpt,${proxyUSAField}`,
-      `DOMAIN-KEYWORD,oaistatic,${proxyUSAField}`,
-      `DOMAIN-KEYWORD,gemini,${proxyUSAField}`
+      `RULE-SET,google,${activeProfileName}`,
+      `RULE-SET,proxy,${activeProfileName}`, // 一定放在靠后的内容，包含openai、gemini，这些应该走专属AI代理
+      `RULE-SET,telegramcidr,${activeProfileName},no-resolve`
     )
     return config;
   }
