08f57d79a3cc392a73e54f83201c1d5d84148a4f
xiaoqinvar <17683860320m0@sina.cn>
2024-08-27 16:08:24 +0800

    feat(nest): 优化redis module

--- nestjs/src/common/modules/redis/redis.builder.ts
+++ nestjs/src/common/modules/redis/redis.builder.ts
@@ -2,24 +2,35 @@
  * @Description: redis 动态模块构建器
  * @Date: 2024/8/27 10:14
  */
-import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
+import {
+  ConfigurableModuleBuilder,
+  DynamicModule,
+  Module,
+} from '@nestjs/common';
 import { RedisService } from './redis.service';
 
-const { ConfigurableModuleClass } = new ConfigurableModuleBuilder().build();
-
-@Module({
-  providers: [RedisService],
-  exports: [RedisService],
-})
-export class RedisModule extends ConfigurableModuleClass {
-  static register() {
-    return {
-      global: true,
-      module: RedisModule,
-    };
-  }
-
-  async getName() {
-    return 'RedisModule';
-  }
+export interface IConfig {
+  flag: string;
+  key: string;
 }
+
+export const {
+  ConfigurableModuleClass, // 动态模块需要继承的类
+  MODULE_OPTIONS_TOKEN, // 动态模块注册时的传参标识，用于@Inject(MODULE_OPTIONS_TOKEN)注入给动态模块service
+} = new ConfigurableModuleBuilder()
+  .setExtras(
+    {
+      isGlobal: true,
+    },
+    (definition, extras) => {
+      definition.providers.push(RedisService);
+      return {
+        ...definition,
+        global: extras.isGlobal,
+        exports: [RedisService],
+      };
+    },
+  )
+  .build();
+
+export class RedisModule extends ConfigurableModuleClass {}
