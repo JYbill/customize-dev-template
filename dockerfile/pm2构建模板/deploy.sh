56eec6f439a79b7793bb42ecc6398351bcf3d079
xiaoqinvar <17683860320m0@sina.cn>
2024-10-15 15:55:18 +0800

    refactor(docker): docker deploy script

--- dockerfile/pm2构建模板/deploy.sh
+++ dockerfile/pm2构建模板/deploy.sh
@@ -1,15 +1,10 @@
-# 部署到第三方docker镜像仓库的shell脚本
-echo 'start ...'
+#!/bin/bash
 
-branch_name=`git name-rev --name-only HEAD`
-tag_id=`git rev-parse HEAD`
+# 测试环境
+bash ./docker-build.sh -p test -s
 
-service_name="resource-partal-and-manage-backend"
+# x86环境
+#./docker-build.sh -p x86 -s
 
-docker_tag="${branch_name}_${tag_id}"
-image_id=`docker images ${service_name} | awk '{ print $3 }' | sed -n '2p'`
-
-docker tag ${image_id} registry.cn-hangzhou.aliyuncs.com/weizhujiao/${service_name}:${docker_tag}
-docker push registry.cn-hangzhou.aliyuncs.com/weizhujiao/${service_name}:${docker_tag}
-
-echo 'deploy success!!!'
+# arm环境
+#./docker-build.sh -p arm -s
