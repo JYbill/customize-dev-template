f52ad9396b1af1335d0bd5342ef3d492a3814a52
xiaoqinvar <17683860320m0@sina.cn>
2024-11-07 18:00:33 +0800

    feat: pm2 config

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -1,23 +1,27 @@
 module.exports = {
-  apps: [
-    {
-      name: 'resource-portal-and-manage', // 名称
-      script: './dist/main.js', // nest入口文件
-      cwd: './', // 工作目录
-      // instances: -1, // 默认 -1：cup - 1
-      exec_mode: 'cluster',
-      autorestart: true, // 发生异常自动重启
-      watch: false, // 不启用监听
-      max_restarts: 1, // 最大重启次数
-      max_memory_restart: '2G', // 超出内存即重启
-      error_file: 'logs/err.log',
-      out_file: 'logs/out.log',
-      env: {
+    apps: [
+      {
+        name: 'resource-partal-and-manage', // 名称
+        script: './dist/main.js', // nest入口文件
+        cwd: './', // 工作目录
+        // instances: -1, // 默认 -1：cup - 1
+        exec_mode: 'cluster',
+        autorestart: true, // 发生异常自动重启
+        watch: false, // 不启用监听
+        max_restarts: 1, // 最大重启次数
+        max_memory_restart: '2G', // 超出内存即重启
+  
+        // 日志相关
+        error_file: "logs/err.log",
+        out_file: "logs/out.log",
+        merge_logs: true,
+        log_date_format: "YYYY-MM-DD HH:mm Z",
+  
         // 环境变量
-        ENV: 'production',
-        PORT: 3000,
-        TZ: 'Asia/Shanghai',
+        env: {
+          NODE_ENV: 'production',
+        },
       },
-    },
-  ],
-};
+    ],
+  };
+  
