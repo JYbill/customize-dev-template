09422d971f98b5f32df242c5786dc75141f4d102
xiaoqinvar <17683860320m0@sina.cn>
2025-06-09 17:48:14 +0800

    feat(util): fs utils

--- 工具/fs.util.ts
+++ 工具/fs.util.ts
@@ -2,6 +2,8 @@ import fs from "fs/promises";
 
 import path from "node:path";
 
+import { HttpError } from "#error/http-error.js";
+
 export class FsUtil {
   /**
    * 从source移动到dest，如果不存在指定目录则创建
@@ -26,16 +28,17 @@ export class FsUtil {
 
     if (sourceError) {
       console.error(sourceError);
-      throw new Error("source file is not exist!");
+      throw HttpError.throwServerError("source file is not exist!");
     } else if (destFolderError) {
       if (!autoCreateDestFolder) {
         console.error(destFolderError);
-        throw new Error("dest folder is not exist!");
+        throw HttpError.throwServerError("dest folder is not exist!");
       }
       await fs.mkdir(destFolderPath);
     }
 
-    await fs.rename(source, dest);
+    await fs.copyFile(source, dest);
+    await fs.unlink(source);
     return {
       source,
       dest,
@@ -43,79 +46,23 @@ export class FsUtil {
   }
 
   /**
- * 根据文件名删除临时文件夹目录的文件(都是同步操作效率不高，且需要制定临时目录地址)
- * @param filename
- * @return {Promise<void>}
- */
-static delTempFileByName(filename) {
-    const { logger } = this.ctx;
-    const filePath = path.resolve(this.tempFilePath, filename);
-    try {
-      const fileStat = fs.statSync(filePath);
-      if (fileStat.isFile()) {
-        // console.log(filePath, filename); // debug
-        fs.unlinkSync(filePath);
-      } else {
-        logger.error(filename);
-        logger.error(filePath);
-        this.ctx.throw(400, "delTempFileByName()#删除的是非文件");
-      }
-    } catch (err) {
-      logger.error("delTempFileByName()#该文件不存在，无需删除");
-      logger.error(filePath);
-    }
+   * 移除指定的文件，如果该文件不存在则不做任何操作，如果希望文件不存在抛出错误请使用nodejs fs.rm()
+   */
+  static async remove(path, rmOptions) {
+    if (!path) return;
+    const { dontThrow = true, recursive = true } = rmOptions ?? {};
+    await fs.rm(path, {
+      force: dontThrow,
+      recursive,
+    });
   }
-  
+
   /**
-  * 存储上传的文件流
-  * @param readableStream
-  * @returns
-  * @description
-  *  依赖：
-  *    mime包：文件名读取MIME类型（推荐使用魔数读取）
-  *  需要配置：baseDir(存储文件地址)
-  */
-  static async saveUploadFile(readableStream): Promise<boolean> {
-   return new Promise((resolve) => {
-     const baseDir = this.baseDir;
- 
-     // 随机文件名hash化
-     const splitList = readableStream.filename.split(".");
-     const suffix = splitList[splitList.length - 1];
-     const filename = splitList.reduce((prev, curr, index) => {
-       if (index === 0) {
-         return curr;
-       } else if (index === splitList.length - 1) {
-         return prev;
-       }
-       return prev + "." + curr;
-     }, "");
-     const randomFilename = filename + "." + Math.random().toString(16).slice(2) + "." + suffix;
- 
-     // 通过文件名后缀获取MIME类型
-     const mimeType = mime.getType(randomFilename);
- 
-     // uri
-     const { href } = new URL(path.join(this.uploadTemp, randomFilename), this.ip);
-     console.log(this.uploadTemp, randomFilename);
-     console.log(href);
-     // 流写入
-     const filepath = path.resolve(baseDir, "app", this.uploadTemp, randomFilename);
-     const ws = fs.createWriteStream(filepath);
-     ws.on("close", () =>
-       resolve({
-         filepath,
-         originFileName: readableStream.filename,
-         filename: randomFilename,
-         url: href,
-         mime: mimeType,
-       }),
-     );
-     ws.on("error", (err) => {
-       this.ctx.logger.error(err);
-       this.ctx.throw(400, "saveUploadFile()#文件本地存储错误");
-     });
-     readableStream.pipe(ws);
-   });
- }
+   * 递归创建目录
+   * @param path
+   * @return {Promise<string | undefined>}
+   */
+  static async mkDirRecursive(path) {
+    return fs.mkdir(path, { recursive: true });
+  }
 }
