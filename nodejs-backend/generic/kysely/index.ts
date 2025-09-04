aae199bdc8ea4cf99691e7c60daa1cad434ee183
xiaoqinvar <17683860320m0@sina.cn>
2025-09-04 11:12:58 +0800

    feat(packaging): kysely template

---
+++ nodejs-backend/generic/kysely/index.ts
@@ -0,0 +1,47 @@
+import { CamelCasePlugin, type Compilable, Kysely, MysqlDialect, sql } from "kysely";
+import { type PoolOptions, createPool } from "mysql2";
+import { format } from "sql-formatter";
+
+import { config } from "#config";
+
+import { globalLogger } from "#logger";
+import type { DB } from "#types/database.d.ts";
+
+const logger = globalLogger.child({ fileFlag: "lib/kysely" });
+
+const mysqlConfig = { ...config.mysql.wzj } as PoolOptions;
+
+const dialect = new MysqlDialect({
+  pool: createPool(mysqlConfig),
+});
+
+const db = new Kysely<DB>({
+  dialect,
+  plugins: [new CamelCasePlugin()],
+});
+
+/**
+ * DEBUG调试SQL语句
+ * @param debug
+ */
+const debugSQL = (debug = false) => {
+  return <T extends Compilable>(db: T) => {
+    const params = db.compile().parameters.map(String);
+    if (debug) {
+      logger.info(
+        "DEBUG Raw SQL\n%s",
+        format(db.compile().sql, {
+          language: "mysql",
+          tabWidth: 2,
+          keywordCase: "upper",
+          linesBetweenQueries: 2,
+          params,
+        }),
+      );
+    }
+    return db;
+  };
+};
+export { sql, debugSQL, db };
+export * from "kysely";
+export * from "kysely/helpers/mysql";
