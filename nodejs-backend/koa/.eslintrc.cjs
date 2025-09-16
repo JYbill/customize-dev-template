a24fcde158e59208cf983bc7f050450c154488fd
xiaoqinvar <17683860320m0@sina.cn>
2025-09-16 19:32:08 +0800

    🔥 remove: 删除过时的 ESLint 配置文件

--- nodejs-backend/koa/.eslintrc.cjs
+++ nodejs-backend/koa/.eslintrc.cjs
@@ -1,22 +1,76 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es6: true,
+/**
+ * @file: eslint.config.js
+ * @author: xiaoqinvar
+ * @desc: 最新版本的eslint模板
+ * @dependencies:
+ * @date: 2025-08-26 14:41:53
+ */
+import eslintJS from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginN from "eslint-plugin-n";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import tsESLint from "typescript-eslint";
+import globals from "globals";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default tsESLint.config([
+  eslintJS.configs.recommended,
+  eslintPluginN.configs["flat/recommended-module"],
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:n/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  ...tsESLint.configs.recommendedTypeCheckedOnly,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'max-lines': ['error', {
+        max: 1500,
+        skipBlankLines: true,
+        skipComments: true
+      }],
+      eqeqeq: ["error", "always"],
+      "prefer-const": "off",
+      "no-unneeded-ternary": "error",
+      "no-duplicate-imports": ["error"],
+      "no-dupe-keys": "error",
+      "no-unused-vars": "off",
+      "no-control-regex": "off",
+      "n/no-missing-import": "off",
+      "n/no-process-exit": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-misused-promises": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": "off",
+      // 技术债
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-for-in-array": "warn"
+    },
   },
-  rules: {
-    eqeqeq: ["error", "always"], // 强制使用 === 和 !==
-    "no-duplicate-imports": ["error"], // 不允许重复导入相同的模块
-    "no-unused-vars": "off",
-    "n/file-extension-in-import": ["error", "always"], // 导入相对路径的文件必须指明扩展名
-  },
-};
+  {
+    ignores: [
+      "*.cjs",
+      "*.js",
+      "*.mjs",
+    ]
+  }
+]);
