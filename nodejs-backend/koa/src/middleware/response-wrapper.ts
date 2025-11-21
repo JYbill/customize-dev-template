3d22af56aa323fd07c8c73e3db72250c6c41c548
xiaoqinvar <17683860320m0@sina.cn>
2025-11-21 14:38:40 +0800

    ✨ feat: 更新koa响应中间件

--- nodejs-backend/koa/src/middleware/response-wrapper.ts
+++ nodejs-backend/koa/src/middleware/response-wrapper.ts
@@ -1,3 +1,7 @@
+import { Readable } from "stream";
+
+import type { Context, Next } from "koa";
+
 import { MessageSymbol } from "#enum/symbol.enum.ts";
 import ResponseUtil from "#utils/response.util.ts";
 
@@ -5,7 +9,7 @@ import ResponseUtil from "#utils/response.util.ts";
  * @Description: 响应结果包装中间件
  * @Date: 2024/7/9 13:53
  */
-export default async function (ctx, next) {
+export default async function (ctx: Context, next: Next) {
   ctx.state.wrapper = true; // 默认启用包裹
   await next();
   // 抛出异常时不会执行，异常全部交给最上游的`ExceptionMiddleware`处理
@@ -21,16 +25,11 @@ export default async function (ctx, next) {
   if (contentType.includes("application/zip")) return;
   if (Buffer.isBuffer(ctx.body)) return; // 不处理Buffer
   if (ctx.body === undefined || ctx.body === null) return; // 不处理任何内容都未返回的接口
+  if (Readable.isReadable(ctx.body as Readable)) return; // 如果是流响应则也不额外处理，兼容nodejs/web 可读流、转换流
 
   // 这里只会处理所有响应内容，并包装一层
   const statusCode = ctx.status;
-  const responseData = ctx.body;
-
-  // 自定义失败逻辑
-  if (responseData.success === false) {
-    ctx.body = ResponseUtil.error(responseData.message, statusCode);
-    return;
-  }
+  const responseData = ctx.body as Record<string, any>;
 
   // 自定义消息逻辑
   /*
@@ -41,7 +40,7 @@ export default async function (ctx, next) {
   */
   const symbols = Object.getOwnPropertySymbols(ctx.body);
   if (symbols.includes(MessageSymbol)) {
-    ctx.body = ResponseUtil.success(null, ctx.body[MessageSymbol], statusCode);
+    ctx.body = ResponseUtil.success(null, ctx.body[MessageSymbol] as string, statusCode);
     return;
   }
 
