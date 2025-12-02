f2c1aa2146394b757849105b06db899b27e96c27
xiaoqinvar <17683860320m0@sina.cn>
2025-12-02 21:11:08 +0800

    ✨ feat: 更新 eslint 配置文件

--- 代码格式标准/eslint.config.js
+++ 代码格式标准/eslint.config.js
@@ -37,24 +37,32 @@ export default tsESLint.config([
       },
     },
     rules: {
-      'max-lines': ['error', {
-        max: 1500,
-        skipBlankLines: true,
-        skipComments: true
-      }],
-      eqeqeq: ["error", "always"],
-      "prefer-const": "off",
-      "no-unneeded-ternary": "error",
-      "no-duplicate-imports": ["error"],
-      "no-dupe-keys": "error",
-      "no-unused-vars": "off",
-      "no-control-regex": "off",
+      // eslint
+      "max-lines": [
+        "error",
+        {
+          max: 1500,
+          skipBlankLines: true,
+          skipComments: true,
+        },
+      ],
+      eqeqeq: ["error", "always"], // 强制使用===
+      "no-unneeded-ternary": "error", // 禁用不必要的三元运算符
+      "no-duplicate-imports": ["error"], // 禁用重复导入
+      "no-dupe-keys": "error", // 禁止对象字面量中出现重复的键
+      "no-control-regex": "off", // 允许正则表达式中的控制字符
+
+      // typescript-eslint
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off", // 允许显示any
+      "@typescript-eslint/consistent-type-imports": "error", // 类型导入必须使用type关键字
+      "@typescript-eslint/no-misused-promises": "off", // 允许适合的地方使用promise
+
+      // n
       "n/no-missing-import": "off",
       "n/no-process-exit": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/consistent-type-imports": "error",
-      "@typescript-eslint/no-misused-promises": "off",
+
+      // unused-imports
       "unused-imports/no-unused-imports": "warn",
       "unused-imports/no-unused-vars": "off",
       // 技术债
